3495e2a697207301f3d91607abc1c1c654b731b9
Hana Widyatari <77064807+hanaw23@users.noreply.github.com>
2024-02-09 14:26:39 +0700

    feat: add edit and delete methods

--- app.py
+++ app.py
@@ -18,7 +18,9 @@ CREATE_CUSTOMERS_TABLE = (
 )
 GET_CUSTOMERS_DATA = "SELECT * FROM customers LIMIT 100;"
 GET_CUSTOMER_BY_ID = "SELECT * FROM customers WHERE id = %s;"
-INSERT_CUSTOMER_RETURN_ID = "INSERT INTO customers (name, ig_account, fav_color) VALUES (%s, %s, %s) RETURNING id, name, ig_account, fav_color;"
+INSERT_CUSTOMER_RETURN_DATA = "INSERT INTO customers (name, ig_account, fav_color) VALUES (%s, %s, %s) RETURNING id, name, ig_account, fav_color;"
+UPDATE_CUSTOMER_RETURN_DATA = "UPDATE customers SET name=%s, ig_account=%s, fav_color=%s WHERE id=%s RETURNING id, name, ig_account, fav_color;"
+DELETE_CUSTOMER_RETURN_ID = "DELETE FROM customers WHERE id=%s RETURNING id;"
 
 # routes
 # === CUSTOMERS ===
@@ -73,7 +75,7 @@ def create_customer():
         with connection:
             with connection.cursor() as cursor:
                 cursor.execute(CREATE_CUSTOMERS_TABLE)
-                cursor.execute(INSERT_CUSTOMER_RETURN_ID, (name, ig_account,fav_color))
+                cursor.execute(INSERT_CUSTOMER_RETURN_DATA, (name, ig_account,fav_color))
                 columns = cursor.description
                 data = {}
                 for value in cursor.fetchall():
@@ -85,8 +87,44 @@ def create_customer():
                     raise HTTPException(status_code=404, detail="Customer not found")
     except Exception as error:
         raise HTTPException(status_code=500, detail=str(error))
-    return {"data": data, "message": f"{name} has been created as new customer.", "status_code": 201}
+    return {"data": data, "message": f"{name} has been created as new customer.", "status_code": 200}
             
+# update
+@app.put("/api/update/customer/<id>")
+def update_customer(id):
+    try:
+        data = request.get_json()
+        name = data['name']
+        ig_account = data['ig_account']
+        fav_color = data['fav_color']
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(UPDATE_CUSTOMER_RETURN_DATA, (name, ig_account,fav_color, id))
+                columns = cursor.description
+                data = {}
+                for value in cursor.fetchall():
+                    temp = {}
+                    for (index, col) in enumerate(value):
+                        temp[columns[index][0]] = col
+                    data = temp
+                if not data:
+                    raise HTTPException(status_code=404, detail="Customer not found")
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
+    return {"data": data, "message": f"{name} data has been updated.", "status_code": 200}
 
-
+# delete
+@app.delete("/api/delete/customer/<id>")
+def delete_customer(id):
+    try:
+        with connection:
+            with connection.cursor() as cursor:
+                cursor.execute(DELETE_CUSTOMER_RETURN_ID, (id,))
+                customer_id = ''
+                customer_id = cursor.fetchone()[0]
+                if not customer_id:
+                    raise HTTPException(status_code=404, detail="Customer not found")
+    except Exception as error:
+        raise HTTPException(status_code=500, detail=str(error))
+    return {"id": customer_id, "message": "Customer has been deleted.", "status_code": 200}
 
