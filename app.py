c26109d238aefd8f08ac9bcfb4bf0f05364bc9ec
Hana Widyatari <77064807+hanaw23@users.noreply.github.com>
2024-02-10 13:41:09 +0700

    feat: add sort by name

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ connection = psycopg2.connect(url)
 CREATE_CUSTOMERS_TABLE = (
     "CREATE TABLE IF NOT EXISTS customers (id SERIAL PRIMARY KEY, name TEXT, ig_account TEXT, fav_color TEXT);"
 )
-GET_CUSTOMERS_DATA = "SELECT * FROM customers LIMIT %s OFFSET %s;"
+GET_CUSTOMERS_DATA = "SELECT * FROM customers ORDER BY name ASC LIMIT %s OFFSET %s;"
 GET_CUSTOMERS_COUNT = "SELECT COUNT(*) FROM customers;"
 GET_CUSTOMER_BY_ID = "SELECT * FROM customers WHERE id = %s;"
 INSERT_CUSTOMER_RETURN_DATA = "INSERT INTO customers (name, ig_account, fav_color) VALUES (%s, %s, %s) RETURNING id, name, ig_account, fav_color;"
